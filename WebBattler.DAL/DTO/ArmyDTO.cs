dbd8e8f07a47e20ee8f532ec6f227876d570830f
aram555 <aram.harutyunyan2003@inbox.ru>
2026-01-25 22:13:34 +0400

    Changed ArmyDTO

--- WebBattler.DAL/DTO/ArmyDTO.cs
+++ WebBattler.DAL/DTO/ArmyDTO.cs
@@ -6,7 +6,6 @@ public class ArmyDTO
     public string Name { get; set; }
     public List<UnitDTO> Units { get; set; }
     public string? ParentName { get; set; }
-    public List<ArmyDTO> SubArmies { get; set; }
     public string CountryName { get; set; }
     public string ProvinceName { get; set; }
     public string? CityName { get; set; }
